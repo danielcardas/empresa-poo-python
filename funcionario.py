5b7bc329da6f82aedadff062d4b5eeda05428547
danielcardas <84465908+danielcardas@users.noreply.github.com>
2026-01-03 20:37:18 -0300

    feat: Integra funcionario ao json

--- funcionario.py
+++ funcionario.py
@@ -1,3 +1,6 @@
+import json
+
+
 class Funcionario:
     def __init__(self, nome: str, salario_base: float) -> None:
         self.nome = nome
@@ -8,6 +11,17 @@ class Funcionario:
 
     def __repr__(self) -> str:
         return f'{self.nome} ({self.__class__.__name__})'
+    
+    def criar_funcionario(self):
+        with open('funcionarios.json', 'w') as arquivo:
+            json.dump(self.to_dict(), arquivo)
+
+    def to_dict(self):
+        return {
+            "nome": self.nome,
+            "salario_base": self.salario_base,
+            "cargo": self.__class__.__name__
+        }
 
 
 class Desenvolvedor(Funcionario):
@@ -23,3 +37,8 @@ class Gerente(Funcionario):
 class Estagiario(Funcionario):
     def calcular_salario(self):
         return self.salario_base * 0.80
+    
+
+if __name__ == '__main__':
+    func1 = Desenvolvedor('Daniel', 2000)
+    func1.criar_funcionario()
